6c247c0336d71950558501bad58cda169e3a63ef
minillinim <mike@mikeimelfort.com>
2014-01-06 11:27:56 +1000

    added some simple parsers for nucmer and contigs

--- parsem/ParseM.py
+++ parsem/ParseM.py
@@ -38,59 +38,85 @@ __status__ = "Dev"
 ###############################################################################
 ###############################################################################
 
+class NucMerParser:
+    """Wrapper class for parsing nucmer output"""
+    # constants to make the code more readable
+    _START_1  = 0
+    _END_1    = 1
+    _START_2  = 2
+    _END_2    = 3
+    _LEN_1    = 4
+    _LEN_2    = 5
+    _IDENTITY = 6
+    _ID_1     = 7
+    _ID_2     = 8
+
+    def __init__(self):
+        self.prepped = False
+
+    def readNuc(self, fp):
+        """Read through a nucmer coords file
+
+        this is a generator function
+        """
+        line = None # this is a buffer keeping the last unprocessed line
+        while True: # mimic closure; is it a bad idea?
+            if not self.prepped:
+                # we still need to strip out the header
+                    for l in fp: # search for the first record
+                        if l[0] == '=': # next line is good
+                            self.prepped = True
+                            break
+            # file should be prepped now
+            for l in fp:
+                fields = l.split('|')
+                yield ([int(i) for i in fields[0].split()] +
+                       [int(i) for i in fields[1].split()] +
+                       [int(i) for i in fields[2].split()] +
+                       [float(i) for i in fields[3].split()] +
+                       fields[4].split())
+            break # done!
+
+###############################################################################
+###############################################################################
+###############################################################################
+###############################################################################
+
+class ContigParser:
+    """Main class for reading in and parsing contigs"""
+    def __init__(self): pass
+
+    def readFasta(self, fp): # this is a generator function
+        header = None
+        seq = None
+        while True:
+            for l in fp:
+                if l[0] == '>': # fasta header line
+                    if header is not None:
+                        # we have reached a new sequence
+                        yield header, "".join(seq)
+                    header = l.rstrip()[1:].partition(" ")[0] # save the header we just saw
+                    seq = []
+                else:
+                    seq.append(l.rstrip())
+            # anything left in the barrel?
+            if header is not None:
+                yield header, "".join(seq)
+            break
+
+###############################################################################
+###############################################################################
+###############################################################################
+###############################################################################
+
 class TemplateClass():
     """Utilities wrapper"""
     def __init__(self): pass
 
     def sayHi(self):
-        print('write some "REAL" code you bum!')
-    
-    def demoStuff(self):
-        
-        """
-        # parse a file    
-        try:
-            with open(filename, "r") as fh:
-                for line in fh:
-                    print line
-        except: 
-            print "Error opening file:", filename, exc_info()[0]
-            raise    
-        """
-    
-        """
-        fig = plt.figure()
-    
-        #-----
-        # make a 3d plot
-        ax = fig.add_subplot(111, projection='3d')
-        ax.scatter(points[:,0], 
-                   points[:,1],
-                   points[:,2],
-                   #edgecolors='none',
-                   #c=colors,
-                   #s=2,
-                   #marker='.'
-                   )
-        
-        #-----
-        # make a 2d plot
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
-        ax.plot(points[:,0],
-                points[:,1],
-                '*g')
-    
-        #-----
-        # show figure
-        plt.show()  
-        # or save figure
-        plt.savefig(filename,dpi=300,format='png')
-        
-        #-----
-        # clean up!
-        plt.close(fig)
-        del fig
-        """
-    
-        return 0
+        print("ParseM doesn't so anything on it's own. Perhaps we could put some demos here?" )
+
+###############################################################################
+###############################################################################
+###############################################################################
+###############################################################################
